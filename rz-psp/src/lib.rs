576fb46dd40a34d26777e6f99bbfc07ce920558a
Paul Sajna <hello@paulsajna.com>
2024-02-28 18:59:11 -0800

    buncha nasty code I need to rewrite with bytemuck

--- rz-psp/src/lib.rs
+++ rz-psp/src/lib.rs
@@ -11,7 +11,8 @@ use rizin_librz_sys::{
     rz_cmd_status_t_RZ_CMD_STATUS_INVALID,
     RzCmdDescHelp, RzCmdDescArg, rz_cmd_arg_type_t_RZ_CMD_ARG_TYPE_STRING,
     RzCmdStatus, RzCmdDescDetail, RzCmdDescDetailEntry,
-    rz_io_read_at, rz_io_size, RzBinSection, rz_bin_get_sections
+    rz_io_read_at, rz_io_read_at_mapped, rz_io_size, RzBinSection, rz_bin_get_sections,
+    rz_io_p2v,
 };
 
 
@@ -119,8 +120,107 @@ fn do_nid_stuff(core: *mut RzCore) {
     let sections = unsafe { rz_bin_get_sections(bin) };
     let sections = section_list_to_vec(sections);
     for s in sections {
-        println!("{}", unsafe { CStr::from_ptr(s.name).to_str().unwrap()} ); 
+        let name = unsafe { CStr::from_ptr(s.name).to_str().unwrap()};
+        if name == ".rodata.sceModuleInfo" {
+            let mut buf = Vec::new();
+            buf.resize(s.size.try_into().unwrap(), 0);
+            unsafe { rz_io_read_at(io, s.vaddr, buf.as_mut_ptr(), s.size.try_into().unwrap()) };
+            // FIXME
+            let boxed_slice = buf[..core::mem::size_of::<sceModuleInfo>()].to_vec().into_boxed_slice();
+            let boxed_array: Box<[u8; core::mem::size_of::<sceModuleInfo>()]> = boxed_slice.try_into().unwrap();
+            let modinfo = unsafe { core::mem::transmute::<[u8; core::mem::size_of::<sceModuleInfo>()], sceModuleInfo>(*boxed_array)};
+            println!("{:x?}", modinfo);
+
+            buf = Vec::new();
+            buf.resize(modinfo.exports_end_addr as usize - modinfo.exports_addr as usize, 0);
+            let exports_vaddr = unsafe { rz_io_p2v(io, modinfo.exports_addr.try_into().unwrap()) };
+            unsafe { rz_io_read_at(io, exports_vaddr as u64, buf.as_mut_ptr(), (modinfo.exports_end_addr - modinfo.exports_addr) as i32) };
+            let export_bytes = buf;
+
+            println!("export_bytes {:x?}", export_bytes);
+
+            let boxed_slice = export_bytes[..core::mem::size_of::<sceModuleExport>()].to_vec().into_boxed_slice();
+            let boxed_array: Box<[u8; core::mem::size_of::<sceModuleExport>()]> = boxed_slice.try_into().unwrap();
+            let exports = unsafe { core::mem::transmute::<[u8; core::mem::size_of::<sceModuleExport>()], sceModuleExport>(*boxed_array)};
+
+
+            //dbg!(exports);
+            println!("{:x?}", exports);
+            
+            buf = Vec::new();
+            buf.resize(modinfo.imports_end_addr as usize - modinfo.imports_addr as usize, 0);
+            let imports_vaddr = unsafe { rz_io_p2v(io, modinfo.imports_addr.try_into().unwrap()) };
+            unsafe { rz_io_read_at(io, imports_vaddr as u64, buf.as_mut_ptr(), (modinfo.imports_end_addr - modinfo.imports_addr) as i32) };
+            let import_bytes = buf;
+            println!("import_bytes {:x?}", import_bytes);
+
+            let boxed_slice = import_bytes[..core::mem::size_of::<sceModuleImport>()].to_vec().into_boxed_slice();
+            let boxed_array: Box<[u8; core::mem::size_of::<sceModuleImport>()]> = boxed_slice.try_into().unwrap();
+            let imports = unsafe { core::mem::transmute::<[u8; core::mem::size_of::<sceModuleImport>()], sceModuleImport>(*boxed_array)};
+
+            //dbg!(imports);
+            println!("{:x?}", imports);
+            let imports_name_vaddr = unsafe { rz_io_p2v(io, imports.name.try_into().unwrap()) };
+
+            // FIXME strings longer than 256 chars?
+            let mut buf = [0u8; 256];
+            unsafe { rz_io_read_at(io, imports_name_vaddr as u64, buf.as_mut_ptr(), 256);}
+            //dbg!(buf);
+            let import_name = CStr::from_bytes_until_nul(&buf).unwrap().to_str().unwrap();
+            println!("{}", import_name);
+
+
+            let nid_vaddr = unsafe { rz_io_p2v(io, imports.nid_addr.try_into().unwrap()) };
+
+
+            //imports.func_count * 4; 
+            //let mut buf = Vec::new();
+            //buf.resize(imports.func_count as usize * 4, 0);
+            //unsafe { rz_io_read_at(io, nid_vaddr as u64, buf.as_mut_ptr(), imports.func_count as i32 * 4); }
+            //dbg!(buf);  
+
+
+        } else if name == ".rodata.sceNid" {
+            let mut buf = Vec::new();
+            buf.resize(s.size.try_into().unwrap(), 0);
+            unsafe { rz_io_read_at(io, s.vaddr, buf.as_mut_ptr(), s.size.try_into().unwrap()) };
+            println!("sceNid {:x?}", buf);
+        }
+        //println!("{}", name);
     }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct sceModuleInfo {
+    flags: u32,
+    name: [u8; 28],
+    gp_addr: u32,
+    exports_addr: u32,
+    exports_end_addr: u32,
+    imports_addr: u32,
+    imports_end_addr: u32
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct sceModuleImport {
+    name: u32,
+    flags: u32,
+    entry_size: u8,
+    var_count: u8,
+    func_count: u16,
+    nid_addr: u32,
+    funcs_addr: u32,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct sceModuleExport {
+    name: u32,
+    flags: u32,
+    counts: u32,
+    exports: u32
 }
 
 #[no_mangle]
